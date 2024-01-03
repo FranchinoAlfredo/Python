8310228f9b5e870c9c7ec38e8d3345375af76709
FranchinoAlfredo <alfredofranchino89@gmail.com>
2024-01-03 18:07:18 +0100

    prima pubblicazione esercizi python

---
+++ hello_world.py
@@ -0,0 +1,2 @@
+#stampa messaggio hello world
+print("hello world")
---
+++ input&output.py
@@ -0,0 +1,5 @@
+# Chiedi all'utente di inserire qualcosa
+input_utente = input("Inserisci qualcosa: ")
+
+# Stampa l'input ricevuto
+print("Hai inserito:", input_utente)
